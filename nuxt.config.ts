91739a7a35f9be8cb304207e26833b882f36aa12
windlil <spacesouls@163.com>
2023-08-14 15:21:15 +0800

    chore: add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   //自动引入全局css样式
-  css: ['normalize.css', '@/assets/css/global.scss'],
+  css: ['normalize.css', '@/assets/css/global.scss',"@/assets/cus-font/iconfont.css"],
   vite: {
     css: {
       preprocessorOptions: {
